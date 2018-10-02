46468cf6234b6035178d3bfa6912cbf4ba224404
vagrant <nick.von.buttlar@gmail.com>
2018-10-02 03:16:56 +0000

    parsed JSON file and extracted url's

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,40 @@
 var request = require('request');
 var fs = require('fs')
 
+
 console.log('Welcome to the Best GitHub Avatar Downloader on the Web!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+
     headers: {
       'User-Agent': 'request'
+
     }
   };
 
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+
+    var parsedJSON = JSON.parse(body);
+
+    cb(err, parsedJSON);
+
 
   });
 
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, results) {
+
+
   console.log("Errors: ", err);
-  console.log("Result: ", result);
+
+  results.forEach(function(result){
+    console.log(result.avatar_url);
+  })
+
+
 });
