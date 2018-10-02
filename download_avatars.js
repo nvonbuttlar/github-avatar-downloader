193761da01677d5ae97bea71d45143fb23aa845d
vagrant <nick.von.buttlar@gmail.com>
2018-10-02 01:57:36 +0000

    initiated download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
+var fs = require('fs')
 
 console.log('Welcome to the Best GitHub Avatar Downloader on the Web!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors: ", err);
+  console.log("Result: ", result);
+});
