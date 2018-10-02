65ec04309dba4b1a11db8a7f2e4b99fabded744a
vagrant <nick.von.buttlar@gmail.com>
2018-10-02 02:29:44 +0000

    update all files (not secret.js)

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,20 @@ console.log('Welcome to the Best GitHub Avatar Downloader on the Web!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+
+  });
+
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err);
